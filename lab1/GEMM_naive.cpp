20ce577b4b7b1a2fca9fd9b2767db457ec659513
joy <joytown@163.com>
2023-05-28 18:18:36 +0800

    fix:GEMM_naive 修复BUG

--- lab1/GEMM_naive.cpp
+++ lab1/GEMM_naive.cpp
@@ -23,27 +23,32 @@ using namespace std;
 const int MAX_SIZE = 2048;
 
 int m, n, k;
-double a[MAX_SIZE][MAX_SIZE], b[MAX_SIZE][MAX_SIZE], c[MAX_SIZE][MAX_SIZE];
+int a[MAX_SIZE][MAX_SIZE] = {0},
+    b[MAX_SIZE][MAX_SIZE] = {0},
+    c[MAX_SIZE][MAX_SIZE] = {0};
 
-void gemm(double *matA, double *matB, double *matC, const int M, const int N, const int K)
+void gemm(int *matA, int *matB, int *matC, const int M, const int N, const int K)
 {
-    cout << "正在使用二维矩阵乘法一维优化计算..." << endl;
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            double sum = 0;
-            for (int l = 0; l < K; l++)
-            {
-                // printf("A:%.2f * B:%.2f  ", matA[i * K + l], matB[l * N + j]);
-                //  c[i][j] += a[i][l] * b[l][j];  照着二维矩阵按行主序做映射
-                // bug...
-                sum += matA[i * K + l] * matB[l * N + j];
-            }
-            // printf("C:%.2f \n", sum);
-            matC[i * N + j] = sum;
-        }
-    }
+    // cout << "展开为行主序一维数组计算..." << endl;
+    // for (int i = 0; i < M; i++)
+    // {
+    //     for (int j = 0; j < N; j++)
+    //     {
+    //         // int sum = 0;
+    //         matC[i * N + j] = 0;
+    //         for (int k = 0; k < K; k++)
+    //         {
+    //             // bug...
+    //             matC[i * N + j] += matA[i * K + k] * matB[k * N + j];
+    //         }
+    //         // printf("C:%.2f \n", sum);
+    //         // matC[i * N + j] = sum;
+    //     }
+    // }
+
+    // debug..
+    for (int i = 0; i < m * k; i++)
+        std::cout << *matA++ << " ";
 }
 
 void gemm_naive(int M, int N, int K)
@@ -55,11 +60,12 @@ void gemm_naive(int M, int N, int K)
     {
         for (int j = 0; j < N; j++)
         {
-            c[i][j] = 0;
+            int temp = 0;
             for (int l = 0; l < K; l++)
             {
-                c[i][j] += a[i][l] * b[l][j];
+                temp += a[i][l] * b[l][j];
             }
+            c[i][j] = temp;
         }
     }
 }
@@ -71,14 +77,14 @@ void Gen_Matrix(int m, int n, int k)
     {
         for (int j = 0; j < k; j++)
         {
-            a[i][j] = (double)rand() / (double)(RAND_MAX)*10; // 随机产生0～10之间的浮点数
+            a[i][j] = rand() % 11; // 随机产生0～10之间的浮点数
         }
     }
     for (int i = 0; i < k; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            b[i][j] = (double)rand() / (double)(RAND_MAX)*10;
+            b[i][j] = rand() % 11;
         }
     }
 }
@@ -86,24 +92,24 @@ void Gen_Matrix(int m, int n, int k)
 void print_A(int m, int k)
 {
     printf("=======================================================================\n");
-    printf("矩阵A有%d行%d列 :\n", m, k);
+    printf("矩阵A(%d行%d列)\n", m, k);
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < k; j++)
         {
-            printf("%-6.2f\t", a[i][j]);
+            printf("%d\t", a[i][j]);
         }
         printf("\n");
     }
 }
 void print_B(int k, int n)
 {
-    printf("矩阵B有%d行%d列 :\n", k, n);
+    printf("矩阵B(%d行%d列)\n", k, n);
     for (int i = 0; i < k; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            printf("%-6.2f\t", b[i][j]);
+            printf("%d\t", b[i][j]);
         }
         printf("\n");
     }
@@ -111,13 +117,12 @@ void print_B(int k, int n)
 void print_C(int m, int n)
 {
     printf("=======================================================================\n");
-    printf("矩阵C有%d行%d列 :\n", m, n);
+    printf("矩阵C(%d行%d列)\n", m, n);
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-
-            printf("%-6.2f\t", c[i][j]);
+            printf("%d\t", c[i][j]);
         }
         printf("\n");
     }
@@ -127,14 +132,13 @@ int main()
 {
     clock_t start, end;
 
-    printf("请依次输入m,n,k的值（不超过2048，m和n分别为C的行列数，k为A的列数B的行数）:");
+    printf("请依次输入m,n,k的值（不超过2048，m和n分别为C的行列数，k为A的列数和B的行数）:");
     scanf("%d%d%d", &m, &n, &k);
     while (m > 2048 || n > 2048 || k > 2048)
     {
         printf("超出范围，请输入小于2048的值");
         scanf("%d%d%d", &m, &n, &k);
     }
-    // double a[m][m],b[m][m],c[m][m];
 
     // 矩阵初始化
     Gen_Matrix(m, n, k);
@@ -144,7 +148,7 @@ int main()
         print_A(m, k);
         print_B(k, n);
     }
-    // 控制选择哪种方法
+    //  选择GEMM方法
     int choice = 0;
     while (choice != 1 && choice != 2)
     {
@@ -155,13 +159,14 @@ int main()
     if (choice == 1)
         gemm_naive(m, n, k);
     else
-        gemm((double *)&a, (double *)&b, (double *)&c, m, n, k);
+        // bug fix: 展平为一维剧矩阵时不应该取地址
+        gemm((int *)a, (int *)b, (int *)c, m, n, k);
     end = clock();
     double endtime = (double)(end - start) / CLOCKS_PER_SEC;
     // print matrix C
     if (isprint)
     {
-        print_C(m, n);
+        // print_C(m, n);
     }
     printf("GEMM通用矩阵乘法已完成,用时:%f ms.\n", endtime * 1000);
 
